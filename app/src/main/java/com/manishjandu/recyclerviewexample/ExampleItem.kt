cda29a736482028f7e71a75ac956bb4ebcec2fa6
Manish <manishjandu1@gmail.com>
2021-04-27 22:07:22 +0530

    setup recycler view with adapter

---
+++ app/src/main/java/com/manishjandu/recyclerviewexample/ExampleItem.kt
@@ -0,0 +1,7 @@
+package com.manishjandu.recyclerviewexample
+
+data class ExampleItem(
+    val imageResource:Int,
+    val title:String,
+    val subTitle:String
+)
--- app/src/main/java/com/manishjandu/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/manishjandu/recyclerviewexample/MainActivity.kt
@@ -2,10 +2,36 @@ package com.manishjandu.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.security.auth.login.LoginException
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val exampleList:List<ExampleItem> = generateDummyList(500)
+
+        rv.adapter = ExampleAdapter(exampleList)
+        rv.setHasFixedSize(true)
+        rv.layoutManager = LinearLayoutManager(this)
+
+    }
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+        val list = ArrayList<ExampleItem>()
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_android
+                1 -> R.drawable.ic_audio
+                else -> R.drawable.ic_sun
+            }
+            val item = ExampleItem(drawable, "Item $i", "Line 2")
+            list.add(item)
+        }
+        return list
     }
 }
